69fe94505e77cf2ed8cea79d2e8bb8a21a34a915
Christina <christikaes@gmail.com>
2016-04-30 16:16:29 -0400

    fail gracefully

--- ai.js
+++ ai.js
@@ -3,37 +3,50 @@ var alchemyapi = new AlchemyApi();
 
 function getSentiment(text, callback) {
 	alchemyapi.sentiment('text', text, {}, function(response) {
-		return callback(response['docSentiment'].type);
+    if(response['docSentiment'] && response['docSentiment'].type){
+      return callback(response['docSentiment'].type);
+    } else {
+      return callback(null);
+    }
 	});
 }
 
 function getText(text, callback) {
 	alchemyapi.concepts('text', text, { 'showSourceText':1 }, function(response) {
-    return callback(response['concepts'][0].text);
+    if(response['concepts'] && response['concepts'][0] && response['concepts'].text){
+      return callback(response['concepts'][0].text);
+    } else {
+      return callback(null);
+    }
 	});
 }
 
 var analyzeSentiment = function (input, callback) {
   getSentiment(input, function(sentiment){
     getText(input, function(text){
-      var response = {};
+      if(!sentiment || !text){
+        console.log("FAILED")
+        callback("Tell me more.");
+      } else {
+        var response = {};
 
-      // Sentiment
-      response.sentiment = sentiment;
+        // Sentiment
+        response.sentiment = sentiment;
 
-      // Text
-      response.text = "You sound "
-      if(sentiment === "negative"){
-        response.text += "sad, "
-      } else if(sentiment === "positive") {
-        response.text += "happy, "
+        // Text
+        response.text = "You sound "
+        if(sentiment === "negative"){
+          response.text += "sad, "
+        } else if(sentiment === "positive") {
+          response.text += "happy, "
+        }
+        response.text += "tell me more about " + text + ".";
+
+        // Image
+        response.image = "https://www.health-first.org/images/icon_wong_faces_for_pain.gif"
+
+        callback(response);
       }
-      response.text += "tell me more about " + text + ".";
-
-      // Image
-      response.image = "https://www.health-first.org/images/icon_wong_faces_for_pain.gif"
-
-      callback(response);
     })
   })
 }
