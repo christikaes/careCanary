9ee15422b0541db3aa50f023e3e24a750fb4fed5
Christina <christikaes@gmail.com>
2016-04-30 16:05:29 -0400

    cleanup alchemyapi

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var ai = require('./ai.js');
-var alchemy = require('./alchemy');
-
 var app = express();
 
 app.use(function(req, res, next) {
@@ -21,18 +19,15 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.get('/alchemy', function (req, res) {
-  alchemy.analyzeSentiment(req,res);
-});
-
 app.post('/api', function (req, res) {
     var input = req.rawBody;
     console.log("Recieved POST:" + input);
-    var output = ai.getResponse(input);
-    res.send(output);
+    ai.getResponse(input, function(output){
+        res.send(output);
+    });
 });
 
-app.set('port', (process.env.PORT || 80));
+app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function() {
     console.log("Node app is running on port:" + app.get('port'));
 });
