176611f9f2a1c68931b4e9d4b41d471742463326
Christina <christikaes@gmail.com>
2016-04-30 13:32:17 -0400

    add post end point

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+
+app.use(function(req, res, next) {
+  req.rawBody = '';
+  req.setEncoding('utf8');
+  req.on('data', function(chunk) {
+    req.rawBody += chunk;
+  });
+  req.on('end', function() {
+    next();
+  });
+});
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.post('/api', function (req, res) {
+    var text = req.rawBody;
+    res.send(text);
+});
+
+app.set('port', (process.env.PORT || 3000));
+app.listen(app.get('port'), function() {
+    console.log("Node app is running on port:" + app.get('port'));
 });
